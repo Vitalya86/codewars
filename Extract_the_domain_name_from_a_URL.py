1888708d29054ed48e2410d53975f2639d02611a
Vitalya R <denil260686@gmail.com>
2023-05-23 22:23:57 +0600

    add comments

--- Extract_the_domain_name_from_a_URL.py
+++ Extract_the_domain_name_from_a_URL.py
@@ -1,2 +1,6 @@
+# Write a function that when given a URL as a string,
+# parses out just the domain name and returns it as a string.
+# For example:
+
 def domain_name(url):
     return url.split("//")[-1].split("www.")[-1].split(".")[0]
